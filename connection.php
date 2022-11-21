cc5ae51d12591d81ec742b657ff0340bbbf7b496
Ngô Hoàng Phúc <107487039+PhucPhuc1304@users.noreply.github.com>
2022-11-21 17:58:19 +0700

    Phuc

---
+++ connection.php
@@ -0,0 +1,7 @@
+<?php
+    $servername = "quanlygplx.mysql.database.azure.com";
+    $database = "qlgplx";
+    $username = "phuc";
+    $password = "@hutech123";
+    $conn = mysqli_connect($servername, $username, $password, $database);
+?>
